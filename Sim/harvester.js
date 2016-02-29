9e1ee9bbf3642028235a16386c9700e9a1de5b9c
applepie9883 <applepie9883@gmail.com>
2016-02-29 17:48:32 -0500

    Update harvester.js

--- Sim/harvester.js
+++ Sim/harvester.js
@@ -1,21 +1,45 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('harvester'); // -> 'a thing'
- */
- 
-  module.exports = function(creep)
- {
-	 if (creep.harvest(Game.getObjectById(creep.memory.destID)) != OK)
-	 {
-		 var didMove = creep.moveTo(Game.getObjectById(creep.memory.destID));
-		 
-		 if (didMove != OK && didMove != ERR_TIRED)
-		 {
-			 var sources = creep.room.find(FIND_SOURCES);
-			 creep.memory.destID = sources[0].id;
-		 }
-	}
+* Module code goes here. Use 'module.exports' to export things:
+* module.exports = 'a thing';
+*
+* You can import it from another modules like this:
+* var mod = require('harvester'); // -> 'a thing'
+*/
+
+module.exports = function(creep)
+{
+	var sources = creep.room.find(FIND_SOURCES);
+	creep.moveTo(sources[0]);
+	creep.harvest(sources[0]);
 }
+
+
+
+
+
+
+
+
+
+
+/*
+* Module code goes here. Use 'module.exports' to export things:
+* module.exports = 'a thing';
+*
+* You can import it from another modules like this:
+* var mod = require('harvester'); // -> 'a thing'
+*/
+
+/*module.exports = function(creep)
+{
+	if (creep.harvest(Game.getObjectById(creep.memory.destID)) != OK)
+	{
+		var didMove = creep.moveTo(Game.getObjectById(creep.memory.destID));
+		
+		if (didMove != OK && didMove != ERR_TIRED)
+		{
+			var sources = creep.room.find(FIND_SOURCES);
+			creep.memory.destID = sources[0].id;
+		}
+	}
+}*/
