d57d4bc29c1a295797f998be885dbf17a698a2b1
applepie9883 <applepie9883@gmail.com>
2016-03-01 11:53:35 -0500

    Update builder.js

--- Sim/builder.js
+++ Sim/builder.js
@@ -8,19 +8,19 @@
 
 module.exports = function(creep)
 {
- if(creep.carry.energy == 0)
- {
-  creep.moveTo(Game.spawns.Spawn1);
-  Game.spawns.Spawn1.transferEnergy(creep);
- }
- else
- {
-  var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-  
-  if(targets.length)
-  {
-   creep.moveTo(targets[0]);
-   creep.build(targets[0]);
-  }
- }
+	if(creep.carry.energy == 0)
+	{
+		creep.moveTo(Game.spawns.Spawn1);
+		Game.spawns.Spawn1.transferEnergy(creep);
+	}
+	else
+	{
+		var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+		
+		if(targets.length)
+		{
+			creep.moveTo(targets[0]);
+			creep.build(targets[0]);
+		}
+	}
 }
