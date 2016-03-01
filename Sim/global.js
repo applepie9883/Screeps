47d748fa6a7c5070da2294da898c94d47dd58060
applepie9883 <applepie9883@gmail.com>
2016-03-01 11:54:34 -0500

    Update global.js

--- Sim/global.js
+++ Sim/global.js
@@ -1,12 +1,12 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('global'); // -> 'a thing'
- */
- 
- module.exports = defineGlobalVariables();
+* Module code goes here. Use 'module.exports' to export things:
+* module.exports = 'a thing';
+*
+* You can import it from another modules like this:
+* var mod = require('global'); // -> 'a thing'
+*/
+
+module.exports = defineGlobalVariables();
 
 function defineGlobalVariables()
 {
