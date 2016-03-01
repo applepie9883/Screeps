3641d9c0a1f8e267f5348bc1d5f5d598e92c1092
applepie9883 <applepie9883@gmail.com>
2016-03-01 11:50:23 -0500

    Update guarder.js

--- Sim/guarder.js
+++ Sim/guarder.js
@@ -8,20 +8,20 @@
 
 module.exports = function(creep)
 {
- var targets = creep.room.find(FIND_HOSTILE_CREEPS);
- 
- if (targets.length)
- {
-  creep.moveTo(targets[0]);
-  creep.attack(targets[0]);
- }
- else
- {
-  creep.moveTo(Game.spawns.Spawn1);
- }
- 
- if (!creep.getActiveBodyparts(MOVE))
- {
-  creep.suicide();
- }
+	var targets = creep.room.find(FIND_HOSTILE_CREEPS);
+	
+	if (targets.length)
+	{
+		creep.moveTo(targets[0]);
+		creep.attack(targets[0]);
+	}
+	else
+	{
+		creep.moveTo(Game.spawns.Spawn1);
+	}
+	
+	if (!creep.getActiveBodyparts(MOVE))
+	{
+		creep.suicide();
+	}
 }
