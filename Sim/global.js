fee75f689774d5c657459e964623a85b4b2c82f2
applepie9883 <applepie9883@gmail.com>
2016-03-03 17:58:34 -0500

    Update global.js

--- Sim/global.js
+++ Sim/global.js
@@ -11,4 +11,19 @@ module.exports = defineGlobalVariables();
 function defineGlobalVariables()
 {
 	global.ERR_PATH_BLOCKED = -101;
+	
+	global.creepParts = {
+		harvester:
+		[
+			{level:1, cost:150, parts:[WORK, MOVE]},
+			{level:2, cost:250, parts:[WORK, WORK, MOVE]},
+			{level:3, cost:400, parts:[WORK, WORK, WORK, MOVE, MOVE]},
+			{level:4, cost:650, parts:[WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE]}
+		],
+		gatherer:
+		[
+			{level:1, cost:100, parts:[CARRY, MOVE]},
+			{}
+		]
+	};
 }
