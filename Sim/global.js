18827b6f761810a9adf63790755501c2595a43bd
applepie9883 <applepie9883@gmail.com>
2016-03-03 18:07:04 -0500

    Update global.js

--- Sim/global.js
+++ Sim/global.js
@@ -14,12 +14,15 @@ function defineGlobalVariables()
 	
 	global.creepParts = {
 		harvester:
-		[
-			{level:0, cost:150, parts:[WORK, MOVE]},
-			{level:1, cost:250, parts:[WORK, WORK, MOVE]},
-			{level:2, cost:400, parts:[WORK, WORK, WORK, MOVE, MOVE]},
-			{level:3, cost:650, parts:[WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE]}
-		],
+		{
+			level:
+			{
+				1:{level:0, cost:150, parts:[WORK, MOVE]},
+				2:{level:1, cost:250, parts:[WORK, WORK, MOVE]},
+				3:{level:2, cost:400, parts:[WORK, WORK, WORK, MOVE, MOVE]},
+				4:{level:3, cost:650, parts:[WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE]}
+			}
+		},
 		gatherer:
 		[
 			{level:0, cost:100, parts:[CARRY, MOVE]},
